97b9d17f5c81c34fb8590f986bfc19c2f6ad34df
May <hoangquocanh2n6@gmail.com>
2026-08-16 09:39:40 +0700

    SỐ PHÁT LỘC

---
+++ CODE_PTIT/PY01020.py
@@ -0,0 +1,43 @@
+# Một số kết thúc bởi hai chữ số 86 được gọi là số phát lộc. Cho một số nguyên dương không quá 500 chữ số, hãy kiểm tra số đó có phải số phát lộc hay không.
+
+# Input
+
+# Dòng đầu ghi số bộ test. Mỗi bộ test ghi số nguyên dương cần kiểm tra (không quá 500 chữ số)
+
+# Output
+
+# Ghi ra kết quả kiểm tra tương ứng (YES hoặc NO)
+
+# Ví dụ
+
+
+# Input
+
+# Output
+
+# 3
+
+
+# 1539786
+
+
+# 1234789
+
+
+# 8686
+
+
+	
+# YES
+
+
+# NO
+
+
+# YES
+
+t = int(input())
+
+for _ in range(t):
+    s = input()
+    print("YES" if s[-2:] == "86" else "NO")

dcb753dfbf60177ee380f14738012e6325b534c9
May <hoangquocanh2n6@gmail.com>
2026-08-31 22:39:43 +0700

    ĐOẠN CUỐI NGUYÊN TỐ

---
+++ CODE_PTIT/PY01058.py
@@ -0,0 +1,72 @@
+# Cho số nguyên dương N có không quá 500 chữ số.
+
+# Hãy kiểm tra xem 4 chữ số cuối cùng ghép lại có tạo thành một số nguyên tố hay không.
+
+# Chú ý: các chữ số 0 ở đầu trong 4 chữ số cuối vẫn được chấp nhận
+
+# Input
+
+# Dòng đầu ghi số bộ test (không quá 20).
+
+# Mỗi test viết trên một dòng số nguyên dương N, không quá 500 chữ số.
+
+# Output
+
+# Với mỗi bộ test ghi ra YES hoặc NO tùy thuộc kết quả kiểm tra.
+
+# Ví dụ
+
+
+# Input
+
+# Output
+
+# 3
+
+
+# 12234323130097
+
+
+# 3443354654654654461123
+
+
+# 43543543434554659999
+
+
+	
+# YES
+
+
+# YES
+
+
+# NO
+
+import math
+
+def is_prime(n : int) -> bool:
+    if n < 2:
+        return False
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+
+    for i in range(5, int(math.sqrt(n)) + 1, 6):
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+
+    return True
+
+def check(s) -> bool:
+    t = int(s[-4:])
+    return is_prime(t)
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        s = input()
+        print("YES" if check(s) else "NO")
+
+if __name__ == "__main__":
+    main()
